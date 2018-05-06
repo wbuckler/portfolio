565d8a9f31cb84b8a0260a416f9388dd4564331a
wbuckler <35548942+wbuckler@users.noreply.github.com>
2018-05-05 21:21:29 -0600

    Update GSlayerPullTest.js

--- GSlayerPullTest.js
+++ GSlayerPullTest.js
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+//   The five layers presented below will help the user understand how to offer assistance to the Puerto Rican people.
+//In the aftermath of Hurricane Maria, parts of Puerto Rico have become inaccessible due to widespread damage.  By accounting for  
+//aspects of the island that would present the greatest impedance, relief can be rendered.  That's why factoring how water ways 
+//are prone to flooding, affect urban areas and natural habitats, and sources of drinking water are all major considerations that
+//should be noted. 
+
+
+///////////////////////////////////////////////////////////////////////////////
 // set a projection to recall throughout the map
 var projection = ol.proj.get('EPSG:3857');
 
@@ -47,32 +56,88 @@ var basemap_tiled = new ol.layer.Tile({
 		})
 	  ]
 	})
-})
+});
 
 
 
-
-var states_single = new ol.layer.Tile({
-	source: new ol.source.TileWMS({
-		attributions: new ol.Attribution({
-			html: 'State Boundary Restructured - USGS, National Atlas Release 5-14-12'
-		}),
-		params: {'LAYERS':'kbene:statep010'},
-		url: 'http://internetmapping.net:8080/geoserver/wms?',
-		serverType: 'geoserver'
+///////////////////////////////////////////////////////////////////////////////   
+//  Displays natural protected areas.  Although these areas may have already suffered damaged, it would not be the US Army's interest to inflict further manmade damage.
+var states_single = new ol.layer.Image({
+	source: new ol.source.ImageWMS({
+		url: 'http://internetmapping.net:8080/geoserver/kbene/wms?',
+		params: {'LAYERS':'ws_WHB:CLCC_PACAT_PR_PAs_sept2015_version2'},
 	})
-})
+});
 
 
-///////////////////////////////////////////////////////////////////////////////
-// create our base map objects https://wbuckler.github.io/portfolio/   http://mapper.internetmapping.net:8081/geoserver/kbene/wms?    params: {'LAYERS':'kbene:statep010'},
+
+///////////////////////////////////////////////////////////////////////////////   
+//   Displays the anticipated effect of flooding from a dam failure, which is the Lago El Guineo Dam, Orocovis, Puerto Rico, on a sunny day.
+var states_single = new ol.layer.Image({
+	source: new ol.source.ImageWMS({
+//		attributions: new ol.Attribution({
+//			html: 'State Boundary Restructured - USGS, National Atlas Release 5-14-12'
+//		}),
+		url: 'http://internetmapping.net:8080/geoserver/kbene/wms?',
+		params: {'LAYERS':'ws_WHB:guineo_sunny_day'},
+//		serverType: 'geoserver'
+	})
+});
+
+
+
+///////////////////////////////////////////////////////////////////////////////   
+//  Displays a sampling of assessment of water quality at selected wells 
+var states_single = new ol.layer.Image({
+	source: new ol.source.ImageWMS({
+//		attributions: new ol.Attribution({
+//			html: 'State Boundary Restructured - USGS, National Atlas Release 5-14-12'
+//		}),
+		url: 'http://internetmapping.net:8080/geoserver/kbene/wms?',
+		params: {'LAYERS':'ws_WHB:gwqw2016'},
+//		serverType: 'geoserver'
+	})
+});
+
+
+
+///////////////////////////////////////////////////////////////////////////////   
+//   Displays water ways within Puerto Rico
+var states_single = new ol.layer.Image({
+	source: new ol.source.ImageWMS({
+//		attributions: new ol.Attribution({
+//			html: 'State Boundary Restructured - USGS, National Atlas Release 5-14-12'
+//		}),
+		url: 'http://internetmapping.net:8080/geoserver/kbene/wms?',
+		params: {'LAYERS':'ws_WHB:Inland_Waterway_Lines'},
+//		serverType: 'geoserver'
+	})
+});
+
+
+
+///////////////////////////////////////////////////////////////////////////////   
+//Displays sections of Puerto Rico that are urban centers
+var states_single = new ol.layer.Image({
+	source: new ol.source.ImageWMS({
+//		attributions: new ol.Attribution({
+//			html: 'State Boundary Restructured - USGS, National Atlas Release 5-14-12'
+//		}),
+		url: 'http://internetmapping.net:8080/geoserver/kbene/wms?',
+		params: {'LAYERS':'ws_WHB:UrbanizedArea'},
+//		serverType: 'geoserver'
+	})
+});
+
+
+///////////////////////////////////////////////////////////////////////////////   
+// create our base map objects https://wbuckler.github.io/portfolio/   
 var tested = new ol.Map({
 	target: 'testing',
-	layers: [basemap_tiled, states_single], //[basemap_tiled,basemap_bern_tiled,blocks_kml,counties_kml,basemap_tiled,]
-	//layers: [basemap_tiled,basemap_bern_tiled,counties_kml],
+	layers: [basemap_tiled, states_single], 
 	view: new ol.View({
-		center: ol.proj.fromLonLat([-106.6224,35.0849]), // the approximate geographic center of the continental US
-		zoom:5,
+		center: ol.proj.fromLonLat([-66.548375,18.246950]), // the approximate geographic center of the Puerto Rico
+		zoom:9,
 		projection: projection
 		})
 	});
